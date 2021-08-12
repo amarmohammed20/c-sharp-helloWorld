80739ef0f28fd0c12518facb5046a0f48d46783c
Amar Mohammed <63112654+amarmohammed20@users.noreply.github.com>
2021-08-12 15:01:06 +0100

    First Story

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is a comment, same as JavaScript
+
+            Console.Title = "Story Time";
+            Console.WindowHeight = 40;
+
+            Console.WriteLine("A man called .....");
+            Console.ReadLine();
+
+            Console.WriteLine("..... Tried to learn the coding lanuage .....");
+            Console.ReadLine();
+
+            Console.WriteLine("..... He found that his learning was very slow, so he went to work as a .....");
+            Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("..... To late his wife left him for a real coder ;) .....");
+            Console.ReadLine();
+        }
+    }
+}
